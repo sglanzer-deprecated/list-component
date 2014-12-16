a6142ffeb2053205837fd507e03a9c04f1985b5f
sglanzer <sglanzer@gmail.com>
2014-12-15 22:59:42 -0500

    Component with dynamic templates (refined)

--- app/components/dynamic-component.js
+++ app/components/dynamic-component.js
@@ -1,6 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    layoutName: 'dynamic-component',
-    templateName: 'components/default'
+    defaultLayout: function() {
+        var templateName = this.get('customTemplate') || 'optional1';
+        return this.container.lookup('template:components/' + templateName);
+    }.property(),
+
+    actions: {
+        action1: function() {
+            console.log('action1');
+        },
+        action2: function() {
+            console.log('action2');
+        }
+    }
 });
